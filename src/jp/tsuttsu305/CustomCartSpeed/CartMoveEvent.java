4308f62421ae1fda7f88360a0add4fa7c00615d1
tsuttsu305 <tsuttsu305@daiwa-hotcom.com>
2012-11-19 20:43:51 +0900

    Dev

--- src/jp/tsuttsu305/CustomCartSpeed/CartMoveEvent.java
+++ src/jp/tsuttsu305/CustomCartSpeed/CartMoveEvent.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.Listener;
@@ -14,8 +15,10 @@ public class CartMoveEvent implements Listener {
 	public CartMoveEvent(CustomCartSpeed customCartSpeed) {
 		// TODO 自動生成されたコンストラクター・スタブ
 	}
-	
+
 	public void onCartMove(VehicleMoveEvent event){
+		//defaultの速度を指定
+		double setSpeed = 0.4D;
 		Entity cart = event.getVehicle().getVehicle();
 		//EntityがMineCartか判定。
 		if (cart instanceof Minecart){
@@ -27,21 +30,50 @@ public class CartMoveEvent implements Listener {
 				//ディテクターレールを中心として8方位のブロック情報を取得。ちなみに北を基点として時計回り
 				//toBlockが中心
 				Block[] searchBlock = {
-						toBlock.getRelative(BlockFace.NORTH ), 
-						toBlock.getRelative(BlockFace.NORTH_EAST), 
-						toBlock.getRelative(BlockFace.EAST), 
-						toBlock.getRelative(BlockFace.SOUTH_EAST), 
-						toBlock.getRelative(BlockFace.SOUTH), 
-						toBlock.getRelative(BlockFace.SOUTH_WEST), 
-						toBlock.getRelative(BlockFace.WEST), 
-						toBlock.getRelative(BlockFace.NORTH_WEST), 
+						toBlock.getRelative(BlockFace.NORTH ),
+						toBlock.getRelative(BlockFace.NORTH_EAST),
+						toBlock.getRelative(BlockFace.EAST),
+						toBlock.getRelative(BlockFace.SOUTH_EAST),
+						toBlock.getRelative(BlockFace.SOUTH),
+						toBlock.getRelative(BlockFace.SOUTH_WEST),
+						toBlock.getRelative(BlockFace.WEST),
+						toBlock.getRelative(BlockFace.NORTH_WEST),
 						};
 				//複数あった場合の判定用
 				boolean signFound = false;
 				//8方位に看板があるか判定
 				for(int i = 0;i<=7;i++){
 					if(searchBlock[i].getType() == Material.SIGN || searchBlock[i].getType() == Material.SIGN_POST){
-						
+						/*看板は以下の書式
+						 * *****************
+						 * 		[Cart Speed]		*　←小文字でもOKにする。
+						 * 		Max: 0.4			*
+						 * 			null				*
+						 * 			null				*
+						 *******************
+						 */
+						//看板の内容取得
+						String[] signLine = ((Sign)searchBlock[i]).getLines();
+						//1と2行めを判定
+						if (signLine[0].equalsIgnoreCase("[cart speed") && signLine[1].equalsIgnoreCase("max:(\\s|)^\\d*(\\.\\d)?$")){
+							//ダブり判定
+							if(signFound){
+								//ダブリが判明したら処理強制中断
+								return;
+							}else{
+								String[] line2 = signLine[1].split(":");
+								line2[1] = line2[1].replaceAll(" ", "");
+								try {
+									setSpeed = Double.parseDouble(line2[1]);
+								} catch (NumberFormatException e) {
+									// TODO 自動生成された catch ブロック
+									e.printStackTrace();
+									return;
+								}
+								((Minecart) cart).setMaxSpeed(setSpeed);
+
+							}
+						}
 					}
 				}
 			}
